f3d9c601ee25cb530abce1018158f6ff2d9dd9fb
Gregory Beaver <greg@chiaraquartet.net>
2014-01-01 15:21:14 -0600

    more setup testing

--- src/SOM.php
+++ src/SOM.php
@@ -1,4 +1,23 @@
 <?php
-class SOM {
-    
+class SOM
+{
+    private $apikey;
+    private $appid;
+    function __construct()
+    {
+        $data = json_decode(file_get_contents('/home/' . $_SERVER['user'] . '/somchamber.json'));
+        $this->apikey = $data['key'];
+        $this->appid = $data['client'];
+        var_dump($data);
+    }
+
+    function getAPIKey()
+    {
+        
+    }
+
+    function login()
+    {
+        header('Location: https://podio.com/oauth/authorize?client_id=YOUR_APP_ID&redirect_uri=http://chiaraquartet.net/SOM-Chamber-Music');
+    }
 }
