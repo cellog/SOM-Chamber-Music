caa777bc0f5c3d8d4d9a238b9258a31021bbd959
Gregory Beaver <greg@chiaraquartet.net>
2014-01-07 14:46:22 -0600

    too much abstraction, getRegistrations is different for groups

--- src/SOM/Group.php
+++ src/SOM/Group.php
@@ -3,6 +3,24 @@ namespace SOM;
 use PodioItem;
 class Group extends StudentBase
 {
+    function getRegistrations($noretrieve = false)
+    {
+        if ($this->registrations) {
+            return $this->registrations;
+        }
+        $this->retrieve(null, true);
+        foreach ($this->getReferences() as $reference) {
+            if ($reference['field']['external_id'] == 'group') {
+                $ret = array();
+                foreach ($reference['items'] as $item) {
+                    $ret[] = new Registration($reference['items'][0]['item_id'], $noretrieve);
+                }
+                return $ret;
+            }
+        }
+        return array();
+    }
+
     function update()
     {
         $reg = $this->getRegistrations();
--- src/SOM/StudentBase.php
+++ src/SOM/StudentBase.php
@@ -5,28 +5,6 @@ abstract class StudentBase extends Podio
 {
     protected $registrations = null;
     protected $changes = false;
-    function getRegistrations($noretrieve = false)
-    {
-        if ($this->registrations) {
-            return $this->registrations;
-        }
-        $this->retrieve(null, true);
-        foreach ($this->getReferences() as $reference) {
-            if ($reference['field']['external_id'] == 'student') {
-                $reg = new Registration($reference['items'][0]['item_id'], $noretrieve); // dummy, for registered/not registered student
-                $ret = array();
-                if ($noretrieve) {
-                    return $reg;
-                }
-                $info = $reg->getReferences();
-                foreach ($info[0]['items'] as $item) {
-                    $ret[] = new Registration($item['item_id']);
-                }
-                return $ret;
-            }
-        }
-        return array();
-    }
 
     function setRegistrations(array $registrations)
     {
