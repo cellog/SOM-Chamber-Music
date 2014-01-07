bd1c0e6b8fded453db46eedee8c09e375e39979c
Gregory Beaver <greg@chiaraquartet.net>
2014-01-07 14:48:26 -0600

    better test

--- src/SOM/Group.php
+++ src/SOM/Group.php
@@ -10,7 +10,7 @@ class Group extends StudentBase
         }
         $this->retrieve(null, true);
         foreach ($this->getReferences() as $reference) {
-            if ($reference['field']['external_id'] == 'group') {
+            if ($reference['field']['field_id'] == 50176892) {
                 $ret = array();
                 foreach ($reference['items'] as $item) {
                     $ret[] = new Registration($reference['items'][0]['item_id'], $noretrieve);
