9442d8bb4a9be86f804d00fccb011cae43108571
Gregory Beaver <greg@chiaraquartet.net>
2014-01-07 23:32:23 -0600

    add new hooks for passing student id and call number to the registrar's workspace

--- src/SOM/Routes/Workspace/Hooks.php
+++ src/SOM/Routes/Workspace/Hooks.php
@@ -46,6 +46,32 @@ class Hooks extends Route
 
         PodioHook::create('app', $chambergroups->app_id, array('url' => $newgroup, 'type' => 'item.update'));
         echo "done<br>";
+
+        echo "Removing create changes hooks<br>";
+        $hooks = PodioHook::get_for('app', 6453745);
+        foreach ($hooks as $hook) {
+            $hook->delete($hook->id);
+        }
+        echo "done<br>";
+        $newgroup = Hook::prepareUrl('changes', $students, $_POST['student']);
+        echo "Installing create changes hook for students: <strong>", $newgroup,
+             "</strong><br>";
+        PodioHook::create('app', 6453745, array('url' => $newgroup, 'type' => 'item.create'));
+        PodioHook::create('app', 6453745, array('url' => $newgroup, 'type' => 'item.update'));
+        echo "done<br>";
+
+        $newgroup = Hook::prepareUrl('registrategroup', $chambergroups, $_POST['chamber']);
+        echo "Installing update group hook for chamber groups: <strong>", $newgroup,
+             "</strong><br>";
+        PodioHook::create('app', $chambergroups->app_id, array('url' => $newgroup, 'type' => 'item.update'));
+        echo "done<br>";
+
+        $newgroup = Hook::prepareUrl('updatestudent', $students, $_POST['student']);
+        echo "Installing update group hook for students: <strong>", $newgroup,
+             "</strong><br>";
+        PodioHook::create('app', $students->app_id, array('url' => $newgroup, 'type' => 'item.update'));
+        echo "done<br>";
+
         echo '<a href="/SOM-Chamber-Music/index.php/importstudents/', $students->app_id,
              '/', $this->oldspaceid, '/', $chambergroups->app_id,
              '">Continue (Student import)</a>';
