3c5e8f885a20b30401409e964a082d434ac6240f
Gregory Beaver <greg@chiaraquartet.net>
2014-01-10 22:10:35 -0600

    debug

--- src/SOM/Hook.php
+++ src/SOM/Hook.php
@@ -11,6 +11,8 @@ class Hook extends SOM
     function __construct()
     {
         $this->tokens = json_decode(file_get_contents($_SERVER['PWD'] . '/podiotokens.json'), 1);
+        Podio::$logger->log(var_export($this->tokens, 1));
+
         parent::__construct(true);
         if (isset($_SERVER['PATH_INFO'])) {
             $info = explode('/', $_SERVER['PATH_INFO']);
