6358510d3fc9c3f7aaea2692c572dc8d5ef43fdc
Gregory Beaver <greg@chiaraquartet.net>
2014-04-29 15:12:58 -0500

    try again

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php error_reporting(E_ALL);
-echo "0";
+<?php ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 include __DIR__ . '/src/autoload.php'; ?>
     <?php echo "1";
 $som = new SOM; ?>
