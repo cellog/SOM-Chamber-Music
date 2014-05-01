c1364070226e07cc63da83efbdbbd372bf16b7e7
Gregory Beaver <greg@chiaraquartet.net>
2014-04-30 21:46:31 -0500

    make sure we don't accidentally create new student ids

--- src/SOM/Routes/Workspace/Studentimport.php
+++ src/SOM/Routes/Workspace/Studentimport.php
@@ -75,6 +75,7 @@ class Studentimport extends Route
             if (isset($idindices[$name])) {
                 echo "Updating Student ID link<br>";
                 $studentid->id = $idindices[$name]->id;
+                $studentid->retrieve();
             } else {
                 echo "Creating Student ID link<br>";
                 $studentid->fields['id-2'] = 1;
