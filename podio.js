894b5a39f7be5f7ccd002fcee81781e4518a3189
Gregory Beaver <greg@chiaraquartet.net>
2015-04-25 21:31:20 -0500

    begin work on podio authorization

---
+++ podio.js
@@ -0,0 +1,13 @@
+(function () {
+    function podio(clientid, redirecturi) {
+     this.id = clientid
+     this.url = redirecturi
+    }
+    podio.prototype = {
+     constructor: podio,
+     authenticate: function() {
+      var url = "https://podio.com/oauth/authorize?response_type=token&client_id=" +
+       this.id + "&redirect_uri=" + encodeURI(this.url)
+     }
+    }
+})();
