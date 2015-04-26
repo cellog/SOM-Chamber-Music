23c86d605348376dd5eed7189f0bf63965df4878
Gregory Beaver <greg@chiaraquartet.net>
2015-04-25 21:33:31 -0500

    try another method for the podio object

--- podio.js
+++ podio.js
@@ -1,13 +1,11 @@
-(function () {
-    function podio(clientid, redirecturi) {
-     this.id = clientid
-     this.url = redirecturi
-    }
-    podio.prototype = {
-     constructor: podio,
-     authenticate: function() {
-      var url = "https://podio.com/oauth/authorize?response_type=token&client_id=" +
-       this.id + "&redirect_uri=" + encodeURI(this.url)
-     }
-    }
-})();
+function podio(clientid, redirecturi) {
+ this.id = clientid
+ this.url = redirecturi
+}
+podio.prototype = {
+ constructor: podio,
+ authenticate: function() {
+  var url = "https://podio.com/oauth/authorize?response_type=token&client_id=" +
+   this.id + "&redirect_uri=" + encodeURI(this.url)
+ }
+}
