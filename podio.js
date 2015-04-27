9cf5580961ce46f5d57023a355e28a515ef37e6d
Gregory Beaver <greg@chiaraquartet.net>
2015-04-26 20:27:59 -0500

    remove all token storage.  Insecure
    As I was implementing storage for the refresh token, I was concerned
    that it may be insecure because I would have to pass the app secret,
    which means I would have to store the app secret in plaintext on
    github, which means anyone could get full access to the students app,
    which I don’t want.

--- podio.js
+++ podio.js
@@ -20,6 +20,11 @@ podio.prototype = {
    location.href = url
   }
  },
+ refreshToken: function(callback) {
+  var url = "https://podio.com/oauth/token?grant_type=refresh_token&client_id=" +
+   this.id + "&client_secret=" + this.s + "&refresh_token=" +
+   this.tokeninfo.refresh_token
+ },
  parseHash: function(hash) {
   var ret = {}, info = hash.slice(1).split('&')
   for (var i = 0; i < info.length; i++) {
@@ -32,10 +37,19 @@ podio.prototype = {
   this.tokeninfo.access_token = localStorage.getItem('podio.access_token')
   this.tokeninfo.token_type = localStorage.getItem('podio.token_type')
   this.tokeninfo.refresh_token = localStorage.getItem('podio.refresh_token')
+  this.tokeninfo.time = localStorage.getItem('podio.time')
  },
  saveTokenInfo: function() {
   localStorage.setItem('podio.access_token', this.tokeninfo.access_token)
   localStorage.setItem('podio.token_type', this.tokeninfo.token_type)
   localStorage.setItem('podio.refresh_token', this.tokeninfo.refresh_token)
+  localStorage.setItem('podio.time', Math.floor(new Date().getTime() / 1000))
+ },
+ post: function(api, body, callback) {
+  d3.xhr('https://api.podio.com' + api, 'application/json')
+    .post(JSON.stringify(body), callback)
+ },
+ getStringsStudents: function() {
+  this.post('/item/app/10468839/filter/22482207/', this.displayStudents)
  }
 }
