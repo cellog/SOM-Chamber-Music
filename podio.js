976c0318221496e77fafbf57853d3a3b016b1142
Gregory Beaver <greg@chiaraquartet.net>
2015-04-26 20:35:37 -0500

    try grabbing student data and display it

--- podio.js
+++ podio.js
@@ -1,6 +1,7 @@
 function podio(clientid, redirecturi) {
  this.id = clientid
  this.url = redirecturi
+ this.s = 
  this.retrieveTokeninfo();
  this.authenticate()
 }
@@ -15,15 +16,9 @@ podio.prototype = {
   if (location.hash.length) {
     // parse the hash for token
     this.tokeninfo = this.parseHash(location.hash)
-    this.saveTokenInfo()
   } else {
    location.href = url
   }
- },
- refreshToken: function(callback) {
-  var url = "https://podio.com/oauth/token?grant_type=refresh_token&client_id=" +
-   this.id + "&client_secret=" + this.s + "&refresh_token=" +
-   this.tokeninfo.refresh_token
  },
  parseHash: function(hash) {
   var ret = {}, info = hash.slice(1).split('&')
@@ -32,24 +27,16 @@ podio.prototype = {
   }
   return ret
  },
- retrieveTokeninfo: function() {
-  this.tokeninfo = {}
-  this.tokeninfo.access_token = localStorage.getItem('podio.access_token')
-  this.tokeninfo.token_type = localStorage.getItem('podio.token_type')
-  this.tokeninfo.refresh_token = localStorage.getItem('podio.refresh_token')
-  this.tokeninfo.time = localStorage.getItem('podio.time')
- },
- saveTokenInfo: function() {
-  localStorage.setItem('podio.access_token', this.tokeninfo.access_token)
-  localStorage.setItem('podio.token_type', this.tokeninfo.token_type)
-  localStorage.setItem('podio.refresh_token', this.tokeninfo.refresh_token)
-  localStorage.setItem('podio.time', Math.floor(new Date().getTime() / 1000))
- },
  post: function(api, body, callback) {
   d3.xhr('https://api.podio.com' + api, 'application/json')
     .post(JSON.stringify(body), callback)
  },
  getStringsStudents: function() {
-  this.post('/item/app/10468839/filter/22482207/', this.displayStudents)
+  this.post('/item/app/10468839/filter/22482207/', {
+   limit: 200
+  }, this.displayStudents)
+ },
+ displayStudents: function(error, data) {
+  d3.select('#info').text(data)
  }
 }
